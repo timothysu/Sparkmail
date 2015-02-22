b3e20d6c41e44bfaae6fae00066e6bbb3a1fb82e
timothysu <timmys.soup@gmail.com>
2015-02-22 00:35:13 -0500

    Log more

--- routes/content.js
+++ routes/content.js
@@ -5,7 +5,7 @@ var db = new Store("data",{type:'single'});
 var fs = require('fs');
 
 router.get("/", function(req, res) {
-  console.log(JSON.stringify(req.headers, null, 2));
+  console.log(JSON.stringify(req, null, 2));
 
   db.get(req.query.id, function(err, obj) {
     if(err) {
