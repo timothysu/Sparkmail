82b4d580f29a8c1b6e5b187591a1d2dc31dc845a
Syps <nsypteras@gmail.com>
2015-02-21 19:16:10 -0600

    include module for writing text to img

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var gm = require('gm');
+var paintText = require('./js/paintText');
+
+
+/* Draw text to base image */
+console.log("trying paintText...");
+paintText(gm, 'C:/Users/Nick/Github/canadia/public/img/textBase.png', 'hello world', 12, 6);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', baseImg: './img/textBase.png' });
 });
 
 module.exports = router;
---
+++ routes/js/paintText.js
@@ -0,0 +1,48 @@
+
+	var paintText = function(gm, baseImgPath, incomingTxt, fontSize, spaceSize) {
+        
+        console.log("paintText called");
+		
+        
+        // starting pos of first text line, number of words per line
+		var x = 10, 
+			y = 10,
+			wrdsPerLine = 9;
+        
+		// get array of words in body
+		var wrdArray = incomingTxt.split(' ');
+        
+		// get number of words, number of lines
+		var wrdCount = wrdArray.length,
+			lines = wrdCount / wrdsPerLine,
+			height = lines*70;
+        
+		// resize based on number of lines
+        // debug:: new image written to /public/img/2
+		gm(baseImgPath).resize(480, height).write('C:/Users/Nick/Github/canadia/public/img/2.png', 
+                                                               function(e){
+            if (!e)
+                console.log('good');
+            else
+                console.log(e);
+        });
+
+		// for each line, print out the
+		for (var i=0; i<lines; i+=wrdsPerLine) {
+			for (var j=0; j<wrdArray.length; j++) {
+				gm(baseImgPath).font('Arial.ttf').drawText(x, y, wrdArray[j]).write('C:/Users/Nick/Github/canadia/public/img/2.png', 
+                                                               function(e){
+            if (!e)
+                console.log('good');
+            else
+                console.log(e);
+        });;
+				x += wrdArray[j].length*fontSize + spaceSize;
+			}
+			y += 2*fontSize;
+		}
+	};
+
+// exports this module to global
+module.exports = paintText;
+    
